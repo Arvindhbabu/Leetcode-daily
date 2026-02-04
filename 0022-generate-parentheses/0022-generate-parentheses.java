45a84d2a862b5e8bd6791fe48c18bd8646da0850
Arvindh Babu V <arvindhbabu23@gmail.com>
2026-02-04 10:22:02 +0530

    Time: 2 ms (66.67%), Space: 44.4 MB (76.28%) - LeetHub

---
+++ 0022-generate-parentheses/0022-generate-parentheses.java
@@ -0,0 +1,25 @@
+class Solution {
+
+    public static void RecursiveUtility(String str , int op , 
+                              int cp , int n , List<String> result){
+            if(op == n && cp == n){
+                result.add(str);
+
+            }
+            if(op < n){
+                    RecursiveUtility(str + "(" , op + 1 , cp , n , result);
+            }
+            if(cp < op){
+                    RecursiveUtility(str + ")" , op , cp + 1 , n , result);
+                
+            }
+
+    }
+        
+    public List<String> generateParenthesis(int n) {
+        List<String> result = new ArrayList<>();
+        RecursiveUtility("" , 0 , 0 , n , result);
+        return result;
+        
+    }
+}
