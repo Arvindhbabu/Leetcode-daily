9e03870d30038a00bec0e22f115e729b3a0c107a
Arvindh Babu V <arvindhbabu23@gmail.com>
2026-02-04 05:37:01 +0530

    Time: 0 ms (100%), Space: 19.4 MB (44.41%) - LeetHub

--- 3637-trionic-array-i/3637-trionic-array-i.py
+++ 3637-trionic-array-i/3637-trionic-array-i.py
@@ -1,16 +1,17 @@
 class Solution:
     def isTrionic(self, nums: List[int]) -> bool:
         res, stack = [], []
-        for i in range(1, len(nums)):
-            if nums[i] == nums[i - 1]:
+        for i in range(1,len(nums)):
+            if nums[i] == nums[i-1]:
                 return False
-            if nums[i] > nums[i - 1]:
+            if nums[i] > nums[i-1]:
                 res.append(True)
             else:
                 res.append(False)
-        print(res)
+        #print(res)
         for i in res:
-            if stack and stack[-1] == i:
+            if stack and stack[-1]==i:
                 continue
             stack.append(i)
-        return stack == [True, False, True]
+        return stack==[True,False,True]
+        
